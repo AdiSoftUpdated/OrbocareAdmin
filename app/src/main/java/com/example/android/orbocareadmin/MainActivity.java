4094e23bcc947d98b4d7641e0cf8ce7acb7f63f8
adityagithhub <47607701+adityagithhub@users.noreply.github.com>
2019-04-05 19:27:19 +0530

    05-04-2019 UPDATED

--- app/src/main/java/com/example/android/orbocareadmin/MainActivity.java
+++ app/src/main/java/com/example/android/orbocareadmin/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.android.orbocareadmin;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
 
@@ -15,14 +20,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] ITEMS = {"TSP", "Vendor", "Franchise"};
+        final String[] ITEMS = {"TSP", "Vendor", "Franchise"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ITEMS);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner = findViewById(R.id.spinner1);
         spinner.setAdapter(adapter);
-    }
+
+        // Set the ClickListener for Spinner
+        spinner.setOnItemSelectedListener(new  AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> adapterView,
+                                       View view, int i, long l) {
+                // TODO Auto-generated method stub
+
+                if (i == 1 || i==2){
+                    CardView cardView = (CardView) findViewById(R.id.card_view);
+                    cardView.setVisibility(View.GONE);
+
+                }
 
 
+            }
+            // If no option selected
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+
+
+        });
+
+
+
+
+
+        }
 
 
 
