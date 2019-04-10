ae24d58ff44c200eeda9eeb2a25709a77f5cd6ed
adityagithhub <47607701+adityagithhub@users.noreply.github.com>
2019-04-10 19:15:49 +0530

    10-04-19 updated

--- app/src/main/java/com/example/android/orbocareadmin/LoginActivity.java
+++ app/src/main/java/com/example/android/orbocareadmin/LoginActivity.java
@@ -2,28 +2,56 @@ package com.example.android.orbocareadmin;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText emailEditText,passwordEditText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        new Handler().postDelayed(new Runnable() {
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
 
+        mAuth = FirebaseAuth.getInstance();
 
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                Intent i = new Intent(LoginActivity.this, SplashActivity.class);
-                startActivity(i);
-                finish();
+        button = (Button) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Check if user is signed in (non-null) and update UI accordingly.
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    }
+                });
             }
-        }, 1000);
+        });
+
+
+
+
+
     }
+
 }
 
 
