a27ec12d45a73bbc4d939b7bba67cd3e30df87e8
adityagithhub <47607701+adityagithhub@users.noreply.github.com>
2019-04-11 18:31:55 +0530

    11-04-19 updated

--- app/src/main/java/com/example/android/orbocareadmin/LoginActivity.java
+++ app/src/main/java/com/example/android/orbocareadmin/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,12 +25,13 @@ public class LoginActivity extends AppCompatActivity {
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
-
+        FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
 
         button = (Button) findViewById(R.id.login_button);
@@ -37,15 +39,26 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Check if user is signed in (non-null) and update UI accordingly.
                 FirebaseUser currentUser = mAuth.getCurrentUser();
+
                 mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    }
-                });
-            }
-        });
 
+
+
+
+
+
+                    }
+
+                });
+
+            }
+
+        });
 
 
 
